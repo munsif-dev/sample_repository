56cae721df2afbf065a262ac4c72a6bd9fad2a97
Ahamed Munsif <munsifshaifalikhan@gmail.com>
2025-08-26 08:41:39 +0530

    Refactor login function and add new function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This is the main entry point of the application
 
 
-# I am developer 02,
-def login():
-  print("Welcome to my project")
+# This is from developer 02 
+def function():
+  print("Function")
